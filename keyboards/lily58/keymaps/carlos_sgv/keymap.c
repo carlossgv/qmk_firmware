9b98433212683cbb8d928890749e464fda626021
Carlos Gonzalez <carlossgv@gmail.com>
2022-11-21 15:54:15 -0300

    Added symbols to LOWER layer

--- keyboards/lily58/keymaps/carlos_sgv/keymap.c
+++ keyboards/lily58/keymaps/carlos_sgv/keymap.c
@@ -33,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |   7  |   8  |   9  |  /   |      |  \   |
+ * |      |      |      |      |      |      |                    |   7  |   8  |   9  |  /   |  -_  |  =+  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |   4  |   5  |   6  |  *   |      |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |   4  |   5  |   6  |  *   |      |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------.    ,-------|   1  |   2  |   3  |  -   |      |      |
  * |------+------+------+------+------+------|   [   |    |  Num  |------+------+------+------+------+------|
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
    
 [_LOWER] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_P7, KC_P8,   KC_P9,   KC_PSLS, XXXXXXX, KC_BSLS,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_P4, KC_P5,   KC_P6,   KC_PAST, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_P7, KC_P8,   KC_P9,   KC_PSLS, KC_MINS, KC_EQL,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_P4, KC_P5,   KC_P6,   KC_PAST, XXXXXXX, KC_BSLS,
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_P1, KC_P2,   KC_P3,   KC_PMNS, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NUM,  KC_P0, KC_PDOT, XXXXXXX, KC_PPLS, XXXXXXX, XXXXXXX,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
