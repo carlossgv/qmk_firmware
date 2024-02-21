e27e2dd9a1e4ff66bd137eac4007c54242d82ec8
Carlos Gonzalez <carlossgv@gmail.com>
2024-02-21 16:50:05 -0300

    single quote to be symbol layer when hold

--- keyboards/lily58/keymaps/carlos_sgv/keymap.c
+++ keyboards/lily58/keymaps/carlos_sgv/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,         XXXXXXX,
   KC_TAB,  KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,         KC_P,            KC_MINS,
-  KC_CAPS, LSFT_T(KC_A), KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    LGUI_T(KC_L), RSFT_T(KC_SCLN), KC_QUOT,
+  KC_CAPS, LSFT_T(KC_A), KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    LGUI_T(KC_L), RSFT_T(KC_SCLN), MT(_SYMBOL, KC_QUOT),
   KC_LSFT, KC_Z,         KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX, XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT,       KC_SLSH,         KC_RSFT,
                         XXXXXXX, KC_LCTL, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, XXXXXXX
 ),
