2958d8401df7ef648cf88215725204b0190def7f
Carlos Gonzalez <carlossgv@gmail.com>
2024-02-25 21:36:46 -0300

    remove unsued oled config

--- keyboards/lily58/keymaps/carlos_sgv/config.h
+++ keyboards/lily58/keymaps/carlos_sgv/config.h
@@ -23,6 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 
 #define MASTER_LEFT
+// #define SPLIT_LAYER_STATE_ENABLE
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
@@ -31,13 +32,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define PERMISSIVE_HOLD
 // #define IGNORE_MOD_TAP_INTERRUPT
 
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 27
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
+// #undef RGBLED_NUM
+// #define RGBLIGHT_ANIMATIONS
+// #define RGBLED_NUM 27
+// #define RGBLIGHT_LIMIT_VAL 120
+// #define RGBLIGHT_HUE_STEP 10
+// #define RGBLIGHT_SAT_STEP 17
+// #define RGBLIGHT_VAL_STEP 17
 
 // Underglow
 #undef RGBLED_NUM
