743fab482ada008858b0ae6c8aa170f961e1ea51
Carlos Gonzalez <carlossgv@gmail.com>
2026-01-25 21:00:53 -0300

    feat: interchangeable esc key

--- keyboards/lily58/keymaps/carlossgv/keymap.c
+++ keyboards/lily58/keymaps/carlossgv/keymap.c
@@ -1,11 +1,28 @@
 #include QMK_KEYBOARD_H
 
+#ifdef RGBLIGHT_ENABLE
+#  include "rgblight.h"
+#endif
+
 enum layer_number {
   _QWERTY = 0,
   _LOWER,
   _RAISE,
   _ADJUST,
 };
+
+/*
+ * Toggle this depending on what you decide:
+ *  - 1: Esc tap, Win (LGUI) hold
+ *  - 0: Plain Esc
+ */
+#define ESC_IS_WIN_WHEN_HELD 0
+
+#if ESC_IS_WIN_WHEN_HELD
+#  define ESC_KEY MT(MOD_LGUI, KC_ESC)
+#else
+#  define ESC_KEY KC_ESC
+#endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -26,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  ESC_KEY,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                              KC_LCTL, KC_LALT, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_LGUI
 ),
@@ -41,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |   0  |  .   |      |  +   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /      /       \      \  |      |  0   |      |
- *                   |      |      |      |/      /         \      \ |      |(BSPC)|      |
+ *                   |      |      |      | /      /       \      \  |      |      |      |
+ *                   |      |      |      |/      /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
@@ -50,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NUM,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PAST,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P0,   KC_PDOT, XXXXXXX, KC_PPLS,
-                             _______, _______, _______,  _______, _______,  _______, XXXXXXX,  _______
+                             _______, _______, _______,  _______, _______,  _______, XXXXXXX, _______
 ),
 
 /* RAISE
@@ -91,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, UG_SPDD, UG_SPDU, UG_PREV, UG_NEXT, UG_TOGG,
   KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
@@ -103,12 +120,35 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
+#ifdef RGBLIGHT_ENABLE
+// Turn off RGB after 5 minutes of no keypresses
+#define RGB_IDLE_TIMEOUT_MS (5 * 60 * 1000UL)
+// Slow rainbow: higher = faster, lower = slower (0-255 typical)
+#define RGB_RAINBOW_SPEED  20
+
+static uint32_t rgb_last_activity = 0;
+
+void keyboard_post_init_user(void) {
+  // Ensure RGB is enabled even if EEPROM had it off / in solid color
+  rgblight_enable_noeeprom();
+
+  // Slow speed
+  rgblight_set_speed_noeeprom(RGB_RAINBOW_SPEED);
+
+  // Reset idle timer
+  rgb_last_activity = timer_read32();
+}
+#endif
+
+// SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
 #ifdef OLED_ENABLE
+
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master()) return OLED_ROTATION_180;
   return rotation;
 }
 
+// When you add source files to SRC in rules.mk, you can use functions.
 const char *read_layer_state(void);
 const char *read_logo(void);
 void set_keylog(uint16_t keycode, keyrecord_t *record);
@@ -125,10 +165,11 @@ bool oled_task_user(void) {
   }
   return false;
 }
-#endif
+#endif // OLED_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
+
 #ifdef OLED_ENABLE
     set_keylog(keycode, record);
 #endif
